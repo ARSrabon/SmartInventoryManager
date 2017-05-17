0896420258d78176c10980267eb6b54abbae95bb
ARSrabon <m.arsrabon@gmail.com>
2017-05-18 02:21:48 +0600

    added barcode scanner support.

--- app/src/main/java/io/github/arsrabon/m/smartinventorymanager/CheckInActivity.java
+++ app/src/main/java/io/github/arsrabon/m/smartinventorymanager/CheckInActivity.java
@@ -1,12 +1,24 @@
 package io.github.arsrabon.m.smartinventorymanager;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class CheckInActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    EditText edt_ItemCode;
+    ImageButton btn_barcodeScanner;
+    //qr code scanner object
+    private IntentIntegrator qrScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +28,46 @@ public class CheckInActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("New Check In");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //intializing scan object
+        qrScan = new IntentIntegrator(this);
+
+        edt_ItemCode = (EditText) findViewById(R.id.edt_itemCode);
+        btn_barcodeScanner = (ImageButton) findViewById(R.id.btn_barcodeScanner);
+
+        btn_barcodeScanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //initiating the qr code scan
+                qrScan.initiateScan();
+            }
+        });
+    }
+
+    //Getting the scan results
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            //if qrcode has nothing in it
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+            } else {
+                //if qr contains data
+                try {
+                   edt_ItemCode.setText(result.getContents().toString());
+                    Toast.makeText(this,result.getContents().toString(), Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    //if control comes here
+                    //that means the encoded format not matches
+                    //in this case you can display whatever data is available on the qrcode
+                    //to a toast
+                    Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
--- app/src/main/java/io/github/arsrabon/m/smartinventorymanager/MainActivity.java
+++ app/src/main/java/io/github/arsrabon/m/smartinventorymanager/MainActivity.java
@@ -1,13 +1,24 @@
 package io.github.arsrabon.m.smartinventorymanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Toolbar toolbar;
+    private Toolbar toolbar;
+    private Button btn_checkIn;
+    private Button btn_checkOut;
+    private Button btn_move;
+    private Button btn_stock;
+    private Button btn_transactions;
+    private Button btn_Analytics;
+    private Button btn_buyer;
+    private Button btn_vendor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,17 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Inventory Manager");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        btn_checkIn = (Button) findViewById(R.id.btn_checkIn);
+
+        btn_checkIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,CheckInActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 
